20cc48bebdff03c4ae755c8fd3ca537acf4594a7
webber_wu <webber_wu@hiiir.com>
2015-05-13 11:18:06 +0800

    加入mail

---
+++ myapp/routes/contact.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',                // mail 的伺服器, 這邊我用 Gmail
+  auth:{                           // 當成 server 的 Gmail 帳號
+    user:'nodejsdemo@gmail.com',
+    pass:'20150516'
+  }
+});
+
+router.post('/', function(req, res) {
+  GuestName = req.body.name || 'NO_NAME';
+  GuestEmail = req.body.email || 'NO_EMAIL';
+  GuestMessage = req.body.message || 'NO_MESSAGE';
+
+  var mailOptions = {
+    from: 'nodejsdemo@gmail.com',
+    to: GuestEmail,
+    subject: 'Hello~',
+    text: "Hi~" + GuestName + "，很高興收到您的來信！"
+  }
+
+  transporter.sendMail(mailOptions, function(err, info){
+    if(err){
+      console.error(err);
+    }else{
+      console.log(info);
+      res.redirect('/');
+    }
+  });
+});
+
+module.exports = router;
